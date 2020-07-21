a75c6422230f97f6e02d895f417ac9a34ad0511a
Nicolas Mohnblatt <nicolas.mohnblatt@gmail.com>
2020-07-21 16:36:48 +0100

    re-organised code

--- server.go
+++ server.go
@@ -10,19 +10,25 @@ type server interface {
 	sign(kyber.Point, kyber.Point) (kyber.Point, kyber.Point)
 }
 
+// Local server for testing purposes
 type dummyServer struct {
 	ID int
 	sk kyber.Scalar
 }
 
+func newDummyServer(id int) *dummyServer {
+	return &dummyServer{id, suite.GT().Scalar().Pick(blake2xb.New([]byte("this is a seed" + string(id))))}
+}
+
+func (s dummyServer) sign(pk1, pk2 kyber.Point) (kyber.Point, kyber.Point) {
+	return suite.G1().Point().Mul(s.sk, pk1), suite.G2().Point().Mul(s.sk, pk2)
+}
+
+// TCP server to test a networked version of our service
 type tcpServer struct {
 	ID   int
 	addr string
 	sk   kyber.Scalar
-}
-
-func newDummyServer(id int) *dummyServer {
-	return &dummyServer{id, suite.GT().Scalar().Pick(blake2xb.New([]byte("this is a seed" + string(id))))}
 }
 
 func newTCPServer(id int, addr string) *tcpServer {
@@ -30,6 +36,4 @@ func newTCPServer(id int, addr string) *tcpServer {
 	return &s
 }
 
-func (s dummyServer) sign(pk1, pk2 kyber.Point) (kyber.Point, kyber.Point) {
-	return suite.G1().Point().Mul(s.sk, pk1), suite.G2().Point().Mul(s.sk, pk2)
-}
+// TODO: implement a "sign" method for TCp server (dial, send public keys, perform checks (?), etc)
